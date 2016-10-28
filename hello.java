fc09745f322b4af6542a4fbcdcd70e054d9269a4
janakindlam <janakindlam@gmail.com>
2016-10-28 03:33:28 +0300

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,13 @@
+public class hello {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+	}
+}
+/*
+C:\Users\Jana\Desktop>javac hello.java
+
+C:\Users\Jana\Desktop>java hello
+Hello World!
+*/
